08c1fe4ed49a9680970e2fd7ea34766f508fa26a
Zaturrby <rjkorteschiel@gmail.com>
2016-07-30 17:26:52 +0200

    setup proper node API call

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("test")
+const https = require('https');
+
+var options = {
+  protocol: 'https:',
+  hostname: 'api.github.com',
+  path: '/repos/offcourse/offcourse-roadmap/issues',
+  method: 'GET',
+  headers: {
+    'User-Agent': 'Offcourse-roadmap'
+  }
+};
+
+var req = https.request(options, (response) => {
+  response.setEncoding('utf8');
+  var buffer = "", 
+    data,
+    route;
+
+  response.on("data", function (chunk) {
+    buffer += chunk;
+  }); 
+
+  response.on("end", function (err) {
+    // finished transferring data
+    // dump the raw data
+    console.log(buffer);
+    console.log("\n");
+    data = JSON.parse(buffer);
+  }); 
+});
+
+req.on('error', (e) => {
+  console.log(`problem with request: ${e.message}`);
+});
+
+req.end();
